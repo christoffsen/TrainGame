dff0c8aa0a424b2458ac644972cbaee97d239a0c
christoffsen <christopher.elder8@gmail.com>
2025-01-19 18:24:02 -0800

    track following starting to work

--- Assets/TrainGame/FollowTrack.cs
+++ Assets/TrainGame/FollowTrack.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +10,7 @@ public class FollowTrack : MonoBehaviour
     private Quaternion tempRotRaw;
     private Vector3 tempRot;
     Transform lastNavPoint;
+    Transform navPoint = null;
 
     Vector3 tempPos;
     float speed = 0.1f;
@@ -16,7 +18,7 @@ public class FollowTrack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -37,61 +39,71 @@ public class FollowTrack : MonoBehaviour
 
                 if (navPointCount > 0)
                 {
-
-                    Transform navPoint = null;
-                    try
+                    //go to next navPoint
+                    //when we touch a navPoint, go to the next one
+                    //Transform navPoint = null;
+                    /*
+                    if (navPointIndex >= navPointCount //if there's no next navPoint
+                       || lastNavPoint.position == navPoint.position)
                     {
-                        navPoint = nextTrack.transform.GetChild(navPointIndex);
-                        Debug.Log("first point - " + navPoint);
-                    }
-                    catch (UnityException ue)
-                    {
-                        Debug.Log(ue);
-                        //return;
-                    }
-
-                    if (lastNavPoint == null) //if we're at the first nav point of the track 
-                    {
-                        transform.LookAt(navPoint);
-                        Debug.LogWarning("looking at navPoint");
-                    }
-                    else if (navPoint == null //if there's no next navPoint
-                           || (lastNavPoint != null && IsBetween(tempPos, lastNavPoint.position, navPoint.position, 0.1f))) //OR if we're still on the same nav point
-                    {
-                        // specifically DON'T look at the navPoint, because we might be on top of it
+                        // just keep going
                         Debug.Log("same point: " + navPoint);
-                    }
-                    else //we need to move to the next point
+                    }*/
+                    if (lastNavPoint != null)
                     {
-                        navPointIndex += 1; //assume we're not moving backwards for now
-                        Debug.Log("new point - navPoint " + navPoint);
+                        Debug.LogWarning("lastNavPoint: " + lastNavPoint.transform.position);
+                        Debug.LogWarning("bus: " + tempPos);
+                    }
+                    if (navPoint == null
+                        || ( IsTouching(tempPos, lastNavPoint.transform.position, 1.0f))) //if we're starting the first nav point of the track, or if we're touching the last navPoint we were looking at
+                    {
                         try
                         {
+                            //get the next navPoint
                             navPoint = nextTrack.transform.GetChild(navPointIndex);
-                            transform.LookAt(navPoint);
+                            navPointIndex++;
+                            Debug.Log("updating navPoint to " + navPoint);
                         }
                         catch (UnityException ue)
                         {
-                            Debug.Log("caught exception when getting next point: " + ue.Message);
-                            //keep moving for now, since we're not handling the boundary condition of the end of the nav points
+                            Debug.Log(ue);
                             //return;
                         }
+                        transform.LookAt(navPoint);
+                    }
+                    /*
+                    else if(IsBetween(tempPos, lastNavPoint.transform.position, navPoint.transform.position, 0.1f)) {// if we're still on the same nav point
+
                     }
 
+                    else //we need to move to the next point
+                    {
+                        Debug.LogWarning("not between");
+                        Debug.LogWarning(string.Format("tempPos: {0}, lastNavPoint.transform.position: {1}, navPoint.transform.position: {2}", tempPos, lastNavPoint.transform.position, navPoint.transform.position));
+                    }
+                    */
                     move = transform.TransformDirection(move);
                     transform.position = new Vector3(tempPos.x + move.x, tempPos.y + 0, tempPos.z + move.z); // add move to position
                     //move *= speed;
 
 
-                    //transform.position = new Vector3(nextNavPoint.position.x, tempPos.y, nextNavPoint.position.z);
+                    //transform.position = new Vector3(nextNavPoint.transform.position.x, tempPos.y, nextNavPoint.transform.position.z);
                     tempRot = transform.rotation.eulerAngles;
                     tempRotRaw = transform.rotation;
 
 
                     tempRotRaw.eulerAngles = tempRot;
                     transform.rotation = tempRotRaw;
-                    Debug.Log("Looking at " + navPoint);
+                    if (navPoint == null)
+                    {
+                        Debug.Log("Looking at null navPoint!");
+                    }
+                    else
+                    {
+                        Debug.Log("Looking at " + navPoint);
+                    }
                     lastNavPoint = navPoint;
+
                 }
                 else
                 {
@@ -103,27 +115,27 @@ public class FollowTrack : MonoBehaviour
                     transform.position = new Vector3(tempPos.x + move.x, tempPos.y + 0, tempPos.z + move.z);
                 }
             }
-		}
+        }
         else if (Input.GetKey(KeyCode.X))
-		{
+        {
             Vector3 move = new Vector3(0, 0, 0 - speed); // start from moving to the left
             move = transform.TransformDirection(move);
             transform.position = new Vector3(tempPos.x + move.x, tempPos.y + 0, tempPos.z + move.z); // add move to position
         }
     }
 
-	private void OnCollisionEnter(Collision collision)
-	{
+    private void OnCollisionEnter(Collision collision)
+    {
         Debug.Log("Collision Enter: " + collision.gameObject.name);
         if (collision.gameObject.CompareTag("railroad-track"))
-		{
+        {
             touchingTracks.Enqueue(collision.gameObject);
             navPointIndex = 0;
         }
-	}
+    }
 
-	private void OnCollisionExit(Collision collision)
-	{
+    private void OnCollisionExit(Collision collision)
+    {
         Debug.Log("Collision Exit: " + collision.gameObject.name);
         if (collision.gameObject.CompareTag("railroad-track") && touchingTracks.Contains(collision.gameObject))
         {
@@ -132,7 +144,7 @@ public class FollowTrack : MonoBehaviour
     }
 
     private bool IsBetween(Vector3 position, Vector3 start, Vector3 end, float tolerance = 0.5f)
-	{
+    {
         Vector3 line = end - start;
         Vector3 vector = position - start;
 
@@ -141,5 +153,13 @@ public class FollowTrack : MonoBehaviour
         return projection >= 0f
             && projection <= line.magnitude
             && Mathf.Abs(projection - line.magnitude) <= tolerance;
-	}
+    }
+
+    private bool IsTouching(Vector3 position1, Vector3 position2, float tolerance = 0.5f)
+    {
+        return Math.Abs(position1.x - position2.x) <= tolerance
+               //&& Math.Abs(position1.y - position2.y) <= tolerance
+               && Math.Abs(position1.z - position2.z) <= tolerance;
+
+    }
 }
