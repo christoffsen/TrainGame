56300c383b2cc546cd68bcddb9e30c1124423f70
christoffsen <christopher.elder8@gmail.com>
2025-02-26 23:01:19 -0800

    menu working

---
+++ Assets/TrainGame/StartNewGameButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartNewGameButton : MonoBehaviour
+{
+	public void StartNewGame ()
+	{
+		SceneManager.LoadSceneAsync(1);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
